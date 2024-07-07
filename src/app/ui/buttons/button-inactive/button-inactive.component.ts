832e9b05330117d56818f76af800bdcffc3edcea
RevathyRajeevan <revathyrajeevan12345@gmail.com>
2024-07-07 13:35:38 +0530

    added assessment preview page

--- src/app/ui/buttons/button-inactive/button-inactive.component.ts
+++ src/app/ui/buttons/button-inactive/button-inactive.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button-inactive',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button-inactive.component.html',
   styleUrl: './button-inactive.component.scss'
 })
 export class ButtonInactiveComponent {
   @Input() content: string = '';
+  @Input() customStyles: { [key: string]: string } = {};
+  @Input() customClasses: string[] = [];
 }
