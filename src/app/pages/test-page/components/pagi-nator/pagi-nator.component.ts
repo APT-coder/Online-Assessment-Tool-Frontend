0e6ca59b7703357a189ff1d12a70bcc78e80b720
fardeen-hash <needraf007@gmail.com>
2024-07-11 16:27:40 +0530

    second-commit-working/answered-tracking

--- src/app/pages/test-page/components/pagi-nator/pagi-nator.component.ts
+++ src/app/pages/test-page/components/pagi-nator/pagi-nator.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagi-nator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pagi-nator.component.html',
   styleUrl: './pagi-nator.component.scss'
 })
@@ -11,11 +12,18 @@ export class PagiNatorComponent {
   
   @Input () questions: any;
 
+  @Input() selectedQuestionId: number | null = null;
+
+
   @Output() itemClicked = new EventEmitter<string>();
 
   handleClick(event: Event,item: string): void {
     event.preventDefault();
     this.itemClicked.emit(item);
+    console.log('Selected question id',this.selectedQuestionId);
+
+
+    
   }
 
 }
