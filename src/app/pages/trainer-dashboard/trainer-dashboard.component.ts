532c2e9bffdb4a1d5ede531e759bd9fb00b46fd1
RahulS290600 <mailatrahuls@gmail.com>
2024-07-27 18:44:13 +0530

    Trainer Dashboard Updates

--- src/app/pages/trainer-dashboard/trainer-dashboard.component.ts
+++ src/app/pages/trainer-dashboard/trainer-dashboard.component.ts
@@ -5,21 +5,32 @@ import { TableDashboardComponent } from './components/table-dashboard/table-dash
 import { PieDiagramComponent } from './components/pie-diagram/pie-diagram.component';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../service/user/user.service';
+import { BarGraphComponent } from "./components/bar-graph/bar-graph/bar-graph.component";
 
 @Component({
   selector: 'app-trainer-dashboard',
   standalone: true,
-  imports: [LiveCardComponent, SidebarComponent, TableDashboardComponent, PieDiagramComponent, CommonModule],
+  imports: [LiveCardComponent, SidebarComponent, TableDashboardComponent, PieDiagramComponent, CommonModule, BarGraphComponent],
   templateUrl: './trainer-dashboard.component.html',
   styleUrl: './trainer-dashboard.component.scss'
 })
 export class TrainerDashboardComponent {
   isModalVisible = false;
+  selectedAssessmentId!: number;
+  selectedAssessmentStatus!:string;
 
-  constructor(private userService : UserService) { }
+  constructor(private userService: UserService) {}
 
   showModal() {
     this.isModalVisible = true;
     document.body.classList.add('overlay');
+  }
+
+  onAssessmentSelected(Obj: any) {
+    console.log(Obj);
+    this.selectedAssessmentId = Obj.scheduledAssessmentId;
+    console.log("Assmnt Id" + Obj.scheduledAssessmentId);
+    this.selectedAssessmentStatus = Obj.status;
+    console.log(Obj.status);
   }
 }
